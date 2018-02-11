26a44f2146df8000b9b57b8802d49ef484aaa1f4
Nexao <emilklausen2@hotmail.com>
2018-02-11 23:26:17 +0100

    wee

--- triangles.py
+++ triangles.py
@@ -1,18 +1,20 @@
 print("Input lengths of the triangle sides: ")
-x = int(input("Length of a"))
-y = int(input("Length of b"))
-z = int(input("Length of c"))
+x = int(input("Length of a: "))
+y = int(input("Length of b: "))
+z = int(input("Length of c: "))
 
-if isinstance(x and y and z, int):    
-    if x == y or x == z or y == z:
-        if x == y and x == z:
-            print("Equilateral")
+try:
+    if isinstance(x and y and z, int):    
+        if x == y or x == z or y == z:
+            if x == y and x == z:
+                print("Equilateral")
+            else:
+                print("Isosceles")
         else:
-            print("Isosceles")
+            print("Scalene")
     else:
-        print("Scalene")
-else:
-	print("Input has to be an integer")
-
+        print("Input has to be an integer")
+except ValueError:
+    print("Wops")
 
 
